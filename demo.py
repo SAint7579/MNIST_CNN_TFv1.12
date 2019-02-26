0cbe0a4028bfde7e19e57a357586c205e7b954a4
Vishwa <vishwa20@outlook.com>
2019-02-26 23:33:27 +0530

    Documented demo.py

--- demo.py
+++ demo.py
@@ -14,20 +14,26 @@ with tf.Session() as sess:
     #Loading model and the graphs
     saver = tf.train.import_meta_graph('MNIST_CNN/model-1000.meta')
     saver.restore(sess,'MNIST_CNN/model-1000')
-    
+    #Creating a tf graph object
     graph = tf.get_default_graph()
+    #Restoring placeholders
     x = graph.get_tensor_by_name("x:0")
     y_true = graph.get_tensor_by_name("y_true:0")
-    hold_prob = graph.get_tensor_by_name("hold_prob:0")   
+    hold_prob = graph.get_tensor_by_name("hold_prob:0")
+    #Restoring operation y_pred
     y_pred = graph.get_tensor_by_name("Prediction:0")
     softmax = tf.nn.softmax(y_pred,name="Softmax")
+    #Creating graph
     fig=plt.figure(figsize=(8, 8))
     columns = 4
     rows = 5
     for l in range(1, columns*rows +1):
+        #Selecting a random index
         ind = int(np.random.randint(low= 0,high =500))
         fig.add_subplot(rows, columns, l)
+        #Adding the figure to graph
         plt.imshow(mnist.test.images[ind-1:ind].reshape(28,28),cmap= 'gist_gray')
         pred = (sess.run(tf.argmax(softmax[0]),feed_dict = {x: mnist.test.images[ind-1:ind], y_true: mnist.test.labels[ind-1:ind], hold_prob:1.0}))
+        #Displaying predicted and actual value
         print("Predicted Value: "+ str(pred) + "\tActual Value: "+str(list(mnist.test.labels[ind-1:ind][0]).index(1)))
     plt.show()
